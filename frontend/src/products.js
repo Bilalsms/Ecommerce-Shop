0a4b2511f211f886d8edb3f45fd4dde5a99fa53d
Syed Muhammad Bilal <122206609+Bilalsms@users.noreply.github.com>
2023-08-08 16:08:58 +0500

    Implemented React Router

--- frontend/src/products.js
+++ frontend/src/products.js
@@ -15,7 +15,7 @@ const products = [
 
     {
         _id: 2,
-        name: 'iPhone 11 Pro 256GB Memory',
+        name: 'Apple iPhone 11 Pro 256GB Memory',
         image: '/images/phone.jpg',
         description: 
         'Introducing the iPhone 11 Pro. A transformative triple-camera system that adds tons of capability without complexity. An unprecedented leap in battery life',
