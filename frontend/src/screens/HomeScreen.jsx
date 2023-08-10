24cb2acfd936a72e8aa1efad14ee9e7ca78b1d34
Syed Muhammad Bilal <122206609+Bilalsms@users.noreply.github.com>
2023-08-10 23:08:36 +0500

    Loader and Message Components

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,7 +1,10 @@
 import React from "react";
-import { Row, Col } from "react-bootstrap";
-import Product from "../components/Product";
 import { useGetProductsQuery } from "../slices/productsApiSlice";
+import Loader  from '../components/Loader';
+import Product from "../components/Product";
+import Message from '../components/Message';
+import { Row, Col } from "react-bootstrap";
+
 
 const HomeScreen = () => {
   const { data: products, isLoading, error } = useGetProductsQuery();
@@ -9,9 +12,11 @@ const HomeScreen = () => {
   return (
     <>
       {isLoading ? (
-        <h2>Loading...</h2>
+        <Loader/>
       ) : error ? (
-        <div> {error?.data?.message || error.error} </div>
+        <Message variant='danger'>
+          {error?.data?.message || error.error}
+        </Message> 
       ) : (
         <>
           <h1>Latest Products</h1>
