44f2854e9c94314d1b32169700daa38e99ee1479
Syed Muhammad Bilal <122206609+Bilalsms@users.noreply.github.com>
2023-08-21 18:14:13 +0500

    Admin Functionality: Update users

--- frontend/src/slices/usersApiSlice.js
+++ frontend/src/slices/usersApiSlice.js
@@ -1,5 +1,9 @@
-import { apiSlice } from './apiSlice';
-import { USERS_URL } from '../constants';
+import {
+    apiSlice
+} from './apiSlice';
+import {
+    USERS_URL
+} from '../constants';
 
 export const usersApiSlice = apiSlice.injectEndpoints({
     endpoints: (builder) => ({
@@ -44,9 +48,32 @@ export const usersApiSlice = apiSlice.injectEndpoints({
                 url: `${USERS_URL}/${userId}`,
                 method: "DELETE",
             }),
+        }),
+        getUserDetails: builder.query({
+            query: (userId) => ({
+                url: `${USERS_URL}/${userId}`,
+            }),
+            keepUnusedDataFor: 5,
+        }),
+        updateUser: builder.mutation({
+            query: (data) => ({
+                url: `${USERS_URL}/${data.userId}`,
+                method: "PUT",
+                body: data,
+            }),
+            invalidatesTags: ['Users'],
         })
 
     }),
 });
 
-export const { useLoginMutation, useLogoutMutation, useRegisterMutation, useProfileMutation, useGetUsersQuery, useDeleteUsersMutation } = usersApiSlice;
+export const {
+    useLoginMutation,
+    useLogoutMutation,
+    useRegisterMutation,
+    useProfileMutation,
+    useGetUsersQuery,
+    useDeleteUsersMutation,
+    useGetUserDetailsQuery,
+    useUpdateUserMutation
+} = usersApiSlice;
